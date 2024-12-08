fc5fe84d7a26f74b94707d10b307a7f5cb8561f7
Zaky Zuhair HS <zakyzuhair22@if.unjani.ac.id>
2024-12-08 14:40:22 +0700

    Menambahkan code tugas 3

---
+++ Tugas Modul 5/Tugas Defensive Programming/TDP.java
@@ -0,0 +1,15 @@
+public class TDP {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numerator = 3;
+        int denominator = 0;
+
+        if (denominator == 0) {
+            System.out.println("Error: Pembagian dengan nol tidak diperbolehkan.");
+        } else {
+            System.out.println(numerator / denominator);
+        }
+    }
+}
